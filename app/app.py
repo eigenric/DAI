8b48def7ed62bdab3a224a1c4f4b036c84f045d4
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2022-10-24 17:45:28 +0200

    p2 básica

--- app/app.py
+++ app/app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from bson.json_util import dumps
+from pymongo import MongoClient
+
+from flask import Flask, Response
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
-    
+client = MongoClient("mongo", 27017)
+
+db = client.cockteles
+
+
+@app.route("/todas_las_recetas")
+def mongo():
+    # Encontramos los documentos de la coleccion "recipes"
+    recetas = db.recipes.find() # devuelve un cursor
+
+    lista_recetas = []
+    for receta in recetas:
+        app.logger.debug(receta) # salida consola
+        lista_recetas.append(receta)
+
+    response = {
+        "len": len(lista_recetas),
+        "data": lista_recetas
+    } 
+
+    resJson = dumps(response)
+
+    return Response(resJson, mimetype="application/json")
