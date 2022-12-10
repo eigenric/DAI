7e25cba59469c659eedca84c1682c9c30a2e5e55
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2022-12-10 13:30:15 +0100

    Intento de plantilla

--- app/mi_sitio/recetas/views.py
+++ app/mi_sitio/recetas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    template = loader.get_template('recetas/index.html')
+    return HttpResponse(template.render(request))
