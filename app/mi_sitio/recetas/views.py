88d4dbe9d9b323c68fe0c55353b7a379c67fd351
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2022-12-10 13:42:59 +0100

    Barra de navegación

--- app/mi_sitio/recetas/views.py
+++ app/mi_sitio/recetas/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('recetas/index.html')
-    return HttpResponse(template.render(request))
+    context = dict()
+    return render(request, 'recetas/index.html', context)
